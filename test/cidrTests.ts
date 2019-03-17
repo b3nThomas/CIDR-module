82f74e71dce8cd8e7d756a9f7400c859b10f409d
b3nThomas <b3nadthomas@gmail.com>
2019-03-17 12:09:50 +0000

    Added method and tests to calculate subnet mask

--- test/cidrTests.ts
+++ test/cidrTests.ts
@@ -1,7 +1,7 @@
 import test from 'ava';
-import { cidrIsValid } from '../src/cidr-to-ips';
+import { cidrIsValid, calculateSubnetMask } from '../src/cidr-to-ips';
 
-test('CIDR blocks should be in the correct format', t => {
+test('cidrIsValid checks the validity of a CIDR IP', t => {
     const scenarios = [
         { cidr: '255.255.255.0/24', expected: true },
         { cidr: '0.0.0.0/32', expected: true },
@@ -15,4 +15,15 @@ test('CIDR blocks should be in the correct format', t => {
     });
 });
 
-//
+test('calculateSubnetMask works out the subnet mask based on the number of significant bits', t => {
+    const scenarios = [
+        { cidr: '0.0.0.0/9', expected: '255.128.0.0' },
+        { cidr: '0.0.0.0/16', expected: '255.255.0.0' },
+        { cidr: '0.0.0.0/24', expected: '255.255.255.0' },
+        { cidr: '128.64.32.19/18', expected: '255.255.192.0' }
+    ];
+    scenarios.forEach((s, i) => {
+        const result = calculateSubnetMask(s.cidr);
+        t.deepEqual({ result, i }, { result: s.expected, i });
+    });
+});
